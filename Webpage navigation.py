a5277caaafd98af07426cb1b3a203287ef29fbe2
Hzl0929 <80333221+Hzl0929@users.noreply.github.com>
2024-05-07 21:34:09 +0800

    Create Webpage navigation.py

---
+++ Webpage navigation.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.sparse.linalg
+import utils
+
+#Try some examples.
+P = np.array([ 
+    
+    [0, 0.75, 0.35, 0.25, 0.85], 
+    [0.15, 0, 0.35, 0.25, 0.05], 
+    [0.15, 0.15, 0, 0.25, 0.05], 
+    [0.15, 0.05, 0.05, 0, 0.05], 
+    [0.55, 0.05, 0.25, 0.25, 0]  
+]) 
+
+X0 = np.array([[0],[0],[0],[1],[0]])
+
+### START CODE HERE ###
+
+# Multiply matrix P and X_0 (matrix multiplication).
+X1 = P @ X0
+
+### END CODE HERE ###
+
+print(f'Sum of columns of P: {sum(P)}')
+print(f'X1:\n{X1}')
+
+X = np.array([[0],[0],[0],[1],[0]])
+m = 20
+
+for t in range(m):
+    X = P @ X
+    
+print(X)
+
+eigenvals, eigenvecs = np.linalg.eig(P)
+print(f'Eigenvalues of P:\n{eigenvals}\n\nEigenvectors of P\n{eigenvecs}')
+
+X_inf = eigenvecs[:,0]
+
+print(f"Eigenvector corresponding to the eigenvalue 1:\n{X_inf[:,np.newaxis]}")
+
+# This is organised as a function only for grading purposes.
+def check_eigenvector(P, X_inf):
+    ### START CODE HERE ###
+    X_check = P @ X_inf
+    ### END CODE HERE ###
+    return X_check
+
+X_check = check_eigenvector(P, X_inf)
+print("Original eigenvector corresponding to the eigenvalue 1:\n" + str(X_inf))
+print("Result of multiplication:" + str(X_check))
+
+# Function np.isclose compares two NumPy arrays element by element, allowing for error tolerance (rtol parameter).
+print("Check that PX=X element by element:" + str(np.isclose(X_inf, X_check, rtol=1e-10)))
+
+X_inf = X_inf/sum(X_inf)
+print(f"Long-run probabilities of being at each webpage:\n{X_inf[:,np.newaxis]}")
